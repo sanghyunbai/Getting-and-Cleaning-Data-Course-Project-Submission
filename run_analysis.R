92db6764f4da885935fb04134ead479bc949ac58
sanghyun <sanghyun@kth.se>
2019-02-19 16:54:19 +0100

    run_analysis.R implementation done

--- run_analysis.R
+++ run_analysis.R
@@ -14,37 +14,77 @@ run <- function(){
         activityLabels <- read.table(
                 paste(getwd(),info,"activity_labels.txt",sep = "/")
                 ,header=FALSE);
+        #tag column names 
+        colnames(activityLabels) <- c('activityId','activityType')
+        
         ## read training dataset
         trainingSubject <- read.table(
-                paste(getwd(),train, "subject_train.txt",sep = "/"))
+                paste(getwd(),train, "subject_train.txt",sep = "/")
+                ,header=FALSE)
         trainingX <- read.table(
-                paste(getwd(),train, "X_train.txt",sep = "/"))
+                paste(getwd(),train, "X_train.txt",sep = "/")
+                ,header=FALSE)
         trainingY <- read.table(
-                paste(getwd(),train, "y_train.txt",sep = "/"))
+                paste(getwd(),train, "y_train.txt",sep = "/")
+                ,header=FALSE)
+        # tag training dataset
+        colnames(trainingSubject) = "subjectId"
+        colnames(trainingY) = "activityId"
+        colnames(trainingX) = features[,2]
+
         ## Read Test dataset 
         testSubject <- read.table(
-                paste(getwd(),test, "subject_test.txt",sep = "/"))
+                paste(getwd(),test, "subject_test.txt",sep = "/")
+                ,header=FALSE)
         testX <- read.table(
-                paste(getwd(),test, "X_test.txt",sep = "/"))
+                paste(getwd(),test, "X_test.txt",sep = "/")
+                ,header=FALSE)
         testY <- read.table(
-                paste(getwd(),test, "y_test.txt",sep = "/"))
+                paste(getwd(),test, "y_test.txt",sep = "/")
+                ,header=FALSE)
+        #taging 
+        colnames(testSubject) = "subjectId"
+        colnames(testY) = "activityId"
+        colnames(testX) = features[,2]
         
-        #Give variable names
-        colnames(activityLabels) <- c("activityID","activity")
-        
-        ## Step1. Merges the training and the test sets to create one data set.
-        training <- cbind(trainingSubject,trainingX,trainingY)
-        # head(training)
-        testing <- cbind(testSubject,testX,testY) 
+        ## Merges the training and the test sets to create one data set.
+        training <- cbind(trainingY,trainingSubject,trainingX)
+        testing <- cbind(testY,testSubject,testX) 
         mergedActivityData <-rbind(training,testing)
-        
+
+        ## Extracts only the measurements on the mean and standard deviation for each measurement.
         # give column names, so we can extract only needed columns
         # print(features[,2]) # contains measurement names #observations
-        colnames(mergedActivityData) <- c("subject", "activity",features[, 2])
+        columnNames <- colnames(mergedActivityData)
+        extractColumnes<-grepl("subjectId|activityId|mean|std",columnNames)
+        # print(extractColumnes)
+        mergedActivityData<-mergedActivityData[ ,extractColumnes]
         
-        ##Extracts only the measurements on the mean and standard deviation for each measurement.
-        mergedActivityData<-mergedActivityData[ ,grepl("subject|activity|mean|std",
-                                                       colnames(mergedActivityData))]
-        
+
+        # ## Uses descriptive activity names to name the activities in the data set
+        mergedActivityData <- merge(mergedActivityData, activityLabels,
+                                      by="activityId",
+                                      all.x=TRUE)
+
+        columnNames <- colnames(mergedActivityData)
+        columnNames <- gsub("[\\(\\)-]", "", columnNames)
+        # replace names in columns
+        columnNames <- gsub("^f", "frequency", columnNames)
+        columnNames <- gsub("^t", "time", columnNames)
+        columnNames <- gsub("Acc", "Accelerometer", columnNames)
+        columnNames <- gsub("Gyro", "Gyroscope", columnNames)
+        columnNames <- gsub("Magn", "Magnitude", columnNames)
+        columnNames <- gsub("Freq", "Frequency", columnNames)
+        columnNames <- gsub("mean", "Mean", columnNames)
+        columnNames <- gsub("std", "StandardDeviation", columnNames)
+        # use new labels as column names
+        colnames(mergedActivityData) <- columnNames
+
+        ## From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject
+        groupBy<-group_by(mergedActivityData,subjectId,activityId)
+        mergedActivityDataMean<-summarise_each(groupBy,funs = mean)
+
+        write.table(mergedActivityDataMean, "tidy_data.txt", row.names = FALSE,
+                    quote = TRUE)
 }
 run()
